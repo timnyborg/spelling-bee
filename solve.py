73651d655d222e920e936e2e70ff9cdd6c3971ae
Tim Nyborg <tim.nyborg@gmail.com>
2025-10-18 22:05:23 +0100

    refactor: extract filtering

--- solve.py
+++ solve.py
@@ -1,3 +1,4 @@
+import typing
 import click
 import english_words
 import re
@@ -13,22 +14,20 @@ def validate_letters(ctx, param, value: str):
     raise click.BadParameter(f"Not 7 letters: {value}")
 
 
+def filter_dictionary(letters: str) -> typing.Generator[str]:
+    letter_set = set(letters.lower())
+    yield from (
+        word
+        for word in DICTIONARY
+        if len(word) >= 4 and word.startswith(letters[0]) and not set(word) - letter_set
+    )
+
+
 @click.command()
 @click.argument("letters", callback=validate_letters)
 def solve(letters: str):
     """LETTERS should contain the 7 spelling bee letters, with the middle letter first"""
-    letters = letters.lower()
-    letter_set = set(letters)
-    valid_words = sorted(
-        [
-            word
-            for word in DICTIONARY
-            if len(word) >= 4
-            and word.startswith(letters[0])
-            and not set(word) - letter_set
-        ],
-        key=len,
-    )
+    valid_words = sorted(list(filter_dictionary(letters)), key=len)
     for word in valid_words:
         print(word)
 
